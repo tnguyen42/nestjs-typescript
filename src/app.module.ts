67a500ba5c83f621926b731d02a117a62ea6cc10
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2020-10-05 14:47:43 +0200

    Docker configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from "@nestjs/common";
 import { PostsModule } from "./posts/posts.module";
 import { CatsModule } from "./cats/cats.module";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "@hapi/joi";
 
 @Module({
-	imports: [PostsModule, CatsModule],
+	imports: [
+		PostsModule,
+		CatsModule,
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				POSTGRES_HOST: Joi.string().required(),
+				POSTGRES_PORT: Joi.number().required(),
+				POSTGRES_USER: Joi.string().required(),
+				POSTGRES_PASSWORD: Joi.string().required(),
+				POSTGRES_DB: Joi.string().required(),
+				PORT: Joi.number(),
+			}),
+		}),
+	],
 	controllers: [],
 	providers: [],
 })
