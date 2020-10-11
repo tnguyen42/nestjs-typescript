54a62da575fd3b774bda91fd6f96348b05d1a7a0
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2020-10-11 16:53:08 +0200

    Added hot reload + users

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { join } from "path";
 
 @Module({
 	imports: [
@@ -14,7 +15,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 				username: configService.get("POSTGRES_USER"),
 				password: configService.get("POSTGRES_PASSWORD"),
 				database: configService.get("POSTGRES_DB"),
-				entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+				entities: [join(__dirname, "**", "*.entity.{ts,js}")],
 				autoLoadEntities: true,
 				synchronize: true,
 			}),
