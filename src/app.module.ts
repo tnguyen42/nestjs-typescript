d62b48356927b64928cb1b5529f5a119388e451b
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2020-11-03 12:30:15 +0100

    Installed uuid + upload public file service

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { DatabaseModule } from "./database/database.module";
 				AWS_REGION: Joi.string().required(),
 				AWS_ACCESS_KEY_ID: Joi.string().required(),
 				AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+				AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
 				PORT: Joi.number(),
 			}),
 		}),
