558c831a372c48cd923239e78d1954678ab33383
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2020-11-05 19:04:50 +0100

    Fixed JoinTable bug between category and post

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import Category from "./category.entity";
-import PostEntity from "../posts/post.entity";
 import CategoriesController from "./categories.controller";
 import CategoriesService from "./categories.service";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Category, PostEntity])],
+	imports: [TypeOrmModule.forFeature([Category])],
 	controllers: [CategoriesController],
 	providers: [CategoriesService],
 })
--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -23,7 +23,7 @@ class PostEntity {
 	@ManyToOne(() => User, (author: User) => author.posts)
 	public author: User;
 
-	@ManyToMany(() => Category)
+	@ManyToMany(() => Category, (category: Category) => category.posts)
 	@JoinTable()
 	public categories: Category[];
 }
